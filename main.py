3e5e49e2e8afee0c3818b475afc06ce4aa52a0e1
Константин Онищенко <kostik80_80@mail.ru>
2025-05-26 23:02:19 +0300

    Добавление докстринг в функции

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from src.widget import mask_account_card
+from src.widget import get_date, mask_account_card
 
 if __name__ == "__main__":
     print(mask_account_card("MasterCard 7158300734726758"))
     print(mask_account_card("Счет 73654108430135874305"))
+    print(get_date("2024-03-11T02:26:18.671407"))
--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,10 @@
-from .mask import get_mask_card_number, get_mask_account
+from datetime import datetime
+
+from .mask import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(world: str) -> str:
+    """Функция, принимает строку счет или карту и выводит замаскированный счет или карту в формате строки"""
     res = world.split()
     # Если последний элемент - 16 цифр, это карта
     if len(res[-1]) == 16 and res[-1].isdigit():
@@ -12,3 +15,12 @@ def mask_account_card(world: str) -> str:
         name = " ".join(res[:-1])
         return f"{name} {get_mask_account(res[-1])}"
     return "Номер карты или номер счета должен содержать целые числа"
+
+
+def get_date(date_str: str) -> str:
+    """Функция принимает ввиде строки дату и время и выводи в виде читаемого формата"""
+    try:
+        date_obj = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
+        return date_obj.strftime("%d.%m.%Y")
+    except ValueError:
+        return "Некорректный формат даты"
