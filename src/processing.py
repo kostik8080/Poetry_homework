28e3e8805b920192f78c61957b0c99c71d992202
Константин Онищенко <kostik80_80@mail.ru>
2025-06-06 22:48:13 +0300

    add sort_by_date function

--- src/processing.py
+++ src/processing.py
@@ -7,3 +7,19 @@ def filter_by_state(transactions: list[dict], state: str = 'EXECUTED') -> list[d
         """
     return [transaction for transaction in transactions if transaction.get('state') == state]
 
+def sort_by_date(transactions: list[dict], reverse: bool = True) -> list[dict]:
+    """
+        Сортирует список транзакций по дате.
+
+        :param transactions: Список словарей с транзакциями
+        :param reverse: Если True (по умолчанию), сортирует по убыванию (новые сначала),
+                       если False — по возрастанию (старые сначала)
+        :return: Отсортированный список транзакций
+        """
+    return sorted(
+        transactions,
+        key=lambda x: x['date'],
+        reverse=reverse
+    )
+
+
