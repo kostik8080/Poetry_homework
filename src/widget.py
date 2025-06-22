99e4740802b948c981aef533f12b959a727ec793
Константин Онищенко <kostik80_80@mail.ru>
2025-06-22 20:39:51 +0300

    update mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -5,7 +5,15 @@ from .mask import get_mask_account, get_mask_card_number
 
 def mask_account_card(world: str) -> str:
     """Функция, принимает строку счет или карту и выводит замаскированный счет или карту в формате строки"""
+    if not world or world.isspace():
+        return "Номер карты или номер счета должен содержать целые числа"
+
     res = world.split()
+
+    # Если нет частей после split или последняя часть не содержит цифр
+    if not res or not any(c.isdigit() for c in res[-1]):
+        return "Номер карты или номер счета должен содержать целые числа"
+
     # Если последний элемент - 16 цифр, это карта
     if len(res[-1]) == 16 and res[-1].isdigit():
         name = " ".join(res[:-1])
@@ -14,6 +22,7 @@ def mask_account_card(world: str) -> str:
     elif len(res[-1]) == 20 and res[-1].isdigit():
         name = " ".join(res[:-1])
         return f"{name} {get_mask_account(res[-1])}"
+
     return "Номер карты или номер счета должен содержать целые числа"
 
 
